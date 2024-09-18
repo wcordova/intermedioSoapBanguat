e7a604d7eecc082028af86a7b4a576260a2e581f
Stevendual <110703261+Stevendual@users.noreply.github.com>
2024-09-17 21:02:00 -0600

    Prueba

--- src/main/java/com/umg/edu/gt/progra2/HelloWorld/Dto/TipoCambio.java
+++ src/main/java/com/umg/edu/gt/progra2/HelloWorld/Dto/TipoCambio.java
@@ -2,42 +2,4 @@ package com.umg.edu.gt.progra2.HelloWorld.Dto;
 
 public class TipoCambio {
 
-        private String monedaOrigen;
-        private String monedaDestino;
-        private Double valor;
-
-        public TipoCambio() {
-            super();
-        }
-
-        public TipoCambio(String monedaOrigen, String monedaDestino, Double valor) {
-            super();
-            this.monedaOrigen = monedaOrigen;
-            this.monedaDestino = monedaDestino;
-            this.valor = valor;
-        }
-
-        public String getMonedaOrigen() {
-            return monedaOrigen;
-        }
-
-        public void setMonedaOrigen(String monedaOrigen) {
-            this.monedaOrigen = monedaOrigen;
-        }
-
-        public String getMonedaDestino() {
-            return monedaDestino;
-        }
-
-        public void setMonedaDestino(String monedaDestino) {
-            this.monedaDestino = monedaDestino;
-        }
-
-        public Double getValor() {
-            return valor;
-        }
-
-        public void setValor(Double valor) {
-            this.valor = valor;
-        }
 }
