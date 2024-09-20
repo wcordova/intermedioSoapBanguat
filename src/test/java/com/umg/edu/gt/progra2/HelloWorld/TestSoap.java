36dc59cd9bb71cb4ee8a7827c844526fec664af7
Walter Cordova <wcordova@beesion.com>
2024-09-19 20:41:46 -0600

    Cambios incluyendo test

---
+++ src/test/java/com/umg/edu/gt/progra2/HelloWorld/TestSoap.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umg.edu.gt.progra2.HelloWorld;
+
+import com.umg.edu.gt.progra2.HelloWorld.service.TipoCambioSoapService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author wcordova
+ */
+public class TestSoap {
+    
+    @Autowired
+    private TipoCambioSoapService tipoCambioSoapService;
+    
+    @Test
+    public void testObtenerTipoCambioDia() {
+        System.out.println("init");
+        tipoCambioSoapService.obtenerTipoCambioDia();
+        System.out.println("fin");
+    }
+    
+}
